4059d1323bfa0b45aef4c57f8f1e3f19ef048088
조원장 <jowonjang@jowonjang-ui-MacBookPro.local>
2023-05-10 18:10:22 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const Header = (props) => {
+  return <Text>{props.title}</Text>;
+};
+
+const MyProfile = (props) => {
+  return <Profile name="원장" uri="" />;
+};
+
+const Division = (props) => {
+  return <Text>Division</Text>;
+};
+
+const FriendSection = (props) => {
+  return <Text>FriendSection</Text>;
+};
+
+const Profile = (props) => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        source={{
+          uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+        }}
+        style={{
+          width: 30,
+          height: 30,
+        }}
+      />
+      <Text>지은</Text>
+    </View>
+  );
+};
+
+const FriendList = (props) => {
+  return <Text>FriendList</Text>;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="친구" />
+      <MyProfile />
+      <Division />
+      <FriendSection />
+      <FriendList />
     </View>
   );
 }
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
